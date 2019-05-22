6d8f0ee73cf01350473aef765949003544de0aba
mashudranagithub <mashudranabitm@gmail.com>
2019-05-23 03:10:28 +0600

    Page.php file created

--- wp-content/themes/startwordpress/index.php
+++ wp-content/themes/startwordpress/index.php
@@ -13,13 +13,6 @@
       endif;
     ?>
 
-    <!-- <nav>
-      <ul class="pager">
-        <li><a href="#">Previous</a></li>
-        <li><a href="#">Next</a></li>
-      </ul>
-    </nav> -->
-
   </div><!-- /.blog-main -->
 
 
